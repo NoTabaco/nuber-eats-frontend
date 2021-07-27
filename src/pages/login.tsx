ad5b7fdb4b667ba024cdc7c4b390a8239df35203
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-07-27 20:07:37 +0900

    Updated UI Clonning

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,9 @@
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { Button } from "../components/button";
 import { FormError } from "../components/form-error";
+import nuberLogo from "../images/logo.svg";
 import {
   loginMutation,
   loginMutationVariables,
@@ -26,8 +29,10 @@ export const Login = () => {
     register,
     handleSubmit,
     getValues,
-    formState: { errors },
-  } = useForm<ILoginForm>();
+    formState: { errors, isValid },
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
   const onCompleted = (data: loginMutation) => {
     const {
       login: { ok, token },
@@ -50,12 +55,13 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-md py-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
+    <div className="flex items-center flex-col mt-10 md:mt-24">
+      <div className="w-full max-w-screen-sm flex flex-col items-center px-5">
+        <img src={nuberLogo} className="w-52 mb-10" alt="nuberLogo" />
+        <h4 className="w-full font-normal text-3xl mb-5">Welcome back</h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5 mb-4 w-full"
         >
           <input
             {...register("email", { required: "Email is required" })}
@@ -83,13 +89,17 @@ export const Login = () => {
           {errors.password?.type === "minLength" && (
             <FormError errorMessage={"Password must be more than 10 chars"} />
           )}
-          <button className="btn mt-3">
-            {loading ? "Loading..." : "Log In"}
-          </button>
+          <Button canClick={isValid} loading={loading} actionText={"Log In"} />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
         </form>
+        <div>
+          New to Nuber?{" "}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
