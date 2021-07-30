6a6902766f94cf31cd355bce62fd3caf34f4e960
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-07-30 23:30:29 +0900

    Updated Categories Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -37,12 +37,40 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
-  const { data, loading, error } = useQuery<
+  const { data, loading } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: { restaurantsInput: { page: 1 } },
   });
 
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-32 flex justify-center items-center">
+        <input
+          className="input rounded-md border-0 w-3/12"
+          type="Search"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-xl mx-auto mt-5">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map(category => (
+              <div className="flex flex-col items-center cursor-pointer">
+                <div
+                  key={category.id}
+                  className="w-14 h-14 rounded-full bg-cover hover:bg-gray-100"
+                  style={{ backgroundImage: `url(${category.coverImage})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
