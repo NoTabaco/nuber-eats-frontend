20dac23582415b4500fc88e27cecda4073c1228a
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-08-06 18:43:14 +0900

    Finished Create Account E2E

--- cypress/integration/auth/create-account.ts
+++ cypress/integration/auth/create-account.ts
@@ -17,13 +17,30 @@ describe("Create Account", () => {
   });
 
   it("should be able to create account and login", () => {
+    user.intercept("http://localhost:5000/graphql", req => {
+      const { operationName } = req.body;
+      if (operationName && operationName === "createAccountMutation") {
+        req.reply(res => {
+          res.send({
+            data: {
+              createAccount: {
+                ok: true,
+                error: null,
+                __typename: "CreateAccountOutput",
+              },
+            },
+          });
+        });
+      }
+    });
     user.visit("/create-account");
-    user.findByPlaceholderText(/email/i).type("real2@email.com");
+    user.findByPlaceholderText(/email/i).type("hii@gmail.com");
     user.findByPlaceholderText(/password/i).type("12345");
     user.findByRole("listbox").select("Owner");
     user.findByRole("button").click();
     user.wait(1000);
-    user.findByPlaceholderText(/email/i).type("real2@email.com");
+    user.title().should("eq", "Login | Nuber Eats");
+    user.findByPlaceholderText(/email/i).type("hii@gmail.com");
     user.findByPlaceholderText(/password/i).type("12345");
     user.findByRole("button").click();
     user.window().its("localStorage.nuber-token").should("be.a", "string");
