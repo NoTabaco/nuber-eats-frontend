703c884ef9c0623c33470a713aef47fae47dae84
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-08-22 20:35:35 +0900

    Finished Driver Dashboard

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,6 +8,8 @@ interface ICoords {
 
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
   const onSuccess: PositionCallback = ({
     coords: { latitude, longitude },
   }: GeolocationPosition) => {
@@ -21,15 +23,22 @@ export const Dashboard = () => {
       enableHighAccuracy: true,
     });
   }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
   };
 
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ height: "92vh", width: "100%" }}
+        style={{ height: "50vh", width: "100%" }}
       >
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyDPMAUezMKX6JZDClhcBQlAj3BnRGARjus" }}
@@ -37,7 +46,16 @@ export const Dashboard = () => {
           defaultCenter={{ lat: 34.9, lng: 127.5 }}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-        ></GoogleMapReact>
+        >
+          <div
+            className="text-lg"
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   );
